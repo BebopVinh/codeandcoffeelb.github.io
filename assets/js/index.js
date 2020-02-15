83e1eaef173fda600d025d4e050561aa70ef938f
bebopvinh <vinhdo07@gmail.com>
2020-02-14 23:36:59 -0800

    render more information from meetup JSON

--- assets/js/index.js
+++ assets/js/index.js
@@ -9,10 +9,18 @@ document.addEventListener("DOMContentLoaded", function() {
 		let allEvents = events
 			.slice(0, 2)
 			.map(e => {
-				return `<div class="events row"> ${e.local_time}</div>`
+				return `<div class="events row">
+               <p>Time: ${e.local_time}</p>
+               <p>Venue: ${e.venue_name}</p>
+               <p>Address: ${e.venue_address1}, ${
+					e.venue_city
+				}, ${e.venue_state.toUpperCase()} ${e.venue_zip}</p>
+               <p>URL: ${e.event_url}</p>
+            </div>
+            <br/>`
 			})
 			.join("")
-		let eventDiv = document.getElementById("meetup-api")
+		let eventDiv = document.querySelector("div.meetup-api")
 		eventDiv.innerHTML = allEvents
 	}
 })
