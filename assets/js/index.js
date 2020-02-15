722060030992eb37823bf9f1332b7c7f35d4fd7e
bebopvinh <vinhdo07@gmail.com>
2020-02-14 22:55:43 -0800

    add small rendering of JSON to test

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,16 +1,18 @@
-document.addEventListener("DOMContentLoaded", function () {
-   console.log("Ready!")
-   const url = "https://practical-wilson-757230.netlify.com/index.json"
-   fetch(url)
-      .then(response => response.json())
-      .then(events => renderEvents(events))
+document.addEventListener("DOMContentLoaded", function() {
+	console.log("Ready!")
+	const url = "https://practical-wilson-757230.netlify.com/index.json"
+	fetch(url)
+		.then(response => response.json())
+		.then(events => renderEvents(events))
 
-
-   function renderEvents(events) {
-      let allEvents = events.slice(0, 2).map(e => {
-         return `<div class="events row"> ${e.local_time}</div>`
-      }).join("")
-      let eventDiv = document.getElementById("meetup-api")
-      eventDiv.innerHTML = allEvents
-   }
-});
+	function renderEvents(events) {
+		let allEvents = events
+			.slice(0, 2)
+			.map(e => {
+				return `<div class="events row"> ${e.local_time}</div>`
+			})
+			.join("")
+		let eventDiv = document.getElementById("meetup-api")
+		eventDiv.innerHTML = allEvents
+	}
+})
